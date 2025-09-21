f80954601ec835a71a4d95d8a8bf3ae0aa88794c
shah faheem <shahfaheem31415@gmail.com>
2025-09-21 16:00:44 +0530

    recu file added

---
+++ recu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void test(int n){
+    if(n==0){
+        return;
+    }
+    test(n-1);
+    cout<<n<<endl;
+}
+int main() {
+    test(5);
+    return 0;
+}
